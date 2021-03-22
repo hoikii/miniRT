d2d7ae9979d95000d00e06f3b42e919b8d53068e
kanlee <kanlee@student.42seoul.kr>
2021-03-23 00:16:24 +0900

    fix warning

--- disruption.c
+++ disruption.c
@@ -6,7 +6,7 @@
 /*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 14:44:24 by kanlee            #+#    #+#             */
-/*   Updated: 2021/03/22 22:29:52 by kanlee           ###   ########.fr       */
+/*   Updated: 2021/03/23 00:14:29 by kanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,32 @@ t_color	rainbow(t_rec rec)
 {
 	t_color	local_color;
 	t_vec	n;
+	double	t;
+
 	if (rec.objtype == TYPE_SPHERE)
 		n = v_mul(v_add(rec.normal, v_new(1, 1, 1)), 0.5);
 	else
 		n = v_mul(v_add(rec.point, v_new(1, 1, 1)), 0.5);
-	double t = 0.7;
-	local_color = (t_color){n.x, n.y, n.z};
+	t = fabs(v_dot(rec.normal, rec.raydir));
+	local_color = (t_color){n.x, 1 - n.y, n.z};
+//	local_color.r = 0.7 * t + 0.3 * n.x;
+//	local_color.g = 0.7 * t + 0.3 * n.y;
+//	local_color.b = 0.7 * t + 0.3 * n.z;
 	return (local_color);
 }
 
 t_color	checkerboard(t_rec rec)
 {
 	t_color	local_color;
+	t_vec	tmp;
 
 	local_color = (t_color)BLACK;
 	if (rec.objtype == TYPE_SPHERE)
 	{
-		int a = (int)(fabs(floor(rec.normal.x * 2 ))) % 2;
-		int b = (int)(fabs(floor(rec.normal.y * 2 ))) % 2;
-		int c = (int)(fabs(floor(rec.normal.z * 2 ))) % 2;
-		if (a^b^c)
+		tmp.x = (int)(fabs(floor(rec.normal.x * 2 ))) % 2;
+		tmp.y = (int)(fabs(floor(rec.normal.y * 2 ))) % 2;
+		tmp.z = (int)(fabs(floor(rec.normal.z * 2 ))) % 2;
+		if ((int)tmp.x ^ (int)tmp.y ^ (int)tmp.z)
 			local_color = (t_color)WHITE;
 //		double sines = sin( 2*PI * rec.normal.x) * sin( 2*PI *rec.normal.y) * sin( 2*PI *rec.normal.z);
 //		if (sines < 0)
@@ -48,6 +54,12 @@ t_color	checkerboard(t_rec rec)
 	}
 	else
 	{
+		tmp.x = (int)(fabs(floor(rec.point.x * 2 ))) % 2;
+		tmp.y = (int)(fabs(floor(rec.point.y * 2 ))) % 2;
+		tmp.z = (int)(fabs(floor(rec.point.z * 2 ))) % 2;
+		if ((int)tmp.x ^ (int)tmp.y ^ (int)tmp.z)
+			local_color = (t_color)WHITE;
+/*
 		if (v_dot(rec.normal, v_new(0, 0, 1)))
 			if ((int)(floor(rec.point.x * 5) + floor(rec.point.y*5)) % 2)
 				local_color = (t_color){1,1,1};
@@ -57,6 +69,7 @@ t_color	checkerboard(t_rec rec)
 		if (v_dot(rec.normal, v_new(0, 1, 0)))
 			if ((int)(floor(rec.point.x*5) + floor(rec.point.z*5)) % 2)
 				local_color = (t_color){1,1,1};
+*/
 	}
 	return (local_color);
 }
